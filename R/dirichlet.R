4afff60e269b2e36c519e8b0479614fcdb62fd32
David Kahle <david.kahle@gmail.com>
2016-07-04 21:23:33 -0500

    fix ddirichlet, remove p and q for now

--- R/dirichlet.R
+++ R/dirichlet.R
@@ -21,9 +21,13 @@
 #' @seealso \code{\link{dgamma}}; these functions just wrap the 
 #'   (d/p/q/r)gamma functions.
 #' @name dirichlet
-#' @importFrom stats dgamma pgamma qgamma rgamma
+#' @importFrom stats dgamma pgamma qgamma rgamma dbeta
 #' @examples
-#' 1 + 1
+#' 
+#' ddirichlet(c(.5,.5), c(.5, .5))
+#' dbeta(.5, .5, .5)
+#' 
+#' rdirichlet(10, c(.5, .5))
 #' 
 NULL
 
@@ -33,7 +37,12 @@ NULL
 #' @rdname dirichlet
 #' @export
 ddirichlet <- function(x, alpha, log = FALSE) {
-  log_f <- dchisq(x^2, df, ncp, log = TRUE) + log(2) + log(x)
+  if (is.matrix(x)) {
+    return( apply(x, 1, ddirichlet) )
+  }
+  if( sum(x) != 1 ) return(0L)
+  stopifnot(length(x) == length(alpha))
+  log_f <- sum((alpha - 1)*log(x)) + lgamma(sum(alpha)) - sum(lgamma(alpha))
   if(log) return(log_f)
   exp(log_f)
 }
@@ -41,21 +50,21 @@ ddirichlet <- function(x, alpha, log = FALSE) {
 
 
 
-#' @rdname dirichlet
-#' @export
-pdirichlet <- function(q, alpha, lower.tail = TRUE, log.p = FALSE) {
-  pchisq(q^2, df, ncp, lower.tail, log.p)
-}
+# #' @rdname dirichlet
+# #' @export
+# pdirichlet <- function(q, alpha, lower.tail = TRUE, log.p = FALSE) {
+#   pchisq(q^2, df, ncp, lower.tail, log.p)
+# }
 
 
 
 
 
-#' @rdname dirichlet
-#' @export
-qdirichlet <- function(p, alpha, lower.tail = TRUE, log.p = FALSE) {
-  sqrt( qchisq(p, df, ncp, lower.tail, log.p) )
-}
+# #' @rdname dirichlet
+# #' @export
+# qdirichlet <- function(p, alpha, lower.tail = TRUE, log.p = FALSE) {
+#   sqrt( qchisq(p, df, ncp, lower.tail, log.p) )
+# }
 
 
 
@@ -64,7 +73,9 @@ qdirichlet <- function(p, alpha, lower.tail = TRUE, log.p = FALSE) {
 #' @rdname dirichlet
 #' @export
 rdirichlet <- function(n, alpha) {
-  sapply(alpha, function(n) rgamma(n, ))
+  normalize <- function(.) . / sum(.)
+  samps <- sapply(alpha, function(al) rgamma(n, al, 1))
+  t(apply(samps, 1, normalize))
 }
 
 
